782cf8e84daaeb1275fb2ba5cbf1d34decca4210
viv231912 <schen231912@gmail.com>
2024-07-22 13:58:44 +1000

    updateUI function created

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,27 @@
 const cityForm = document.querySelector('form')
+const card = document.querySelector('.card')
+const details = document.querySelector('.details')
+
+//update UI
+const updateUI = (data) => {
+    const cityDetails = data.cityDetails;
+    const weather = data.weather;
+
+    //update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `
+    //remove the d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none')
+    }
+}
+
 
 //update city function, get city first, then weather 
 const updateCity = async (city) => {
@@ -19,7 +42,6 @@ cityForm.addEventListener('submit',  (e) => {
 
     //update the ui with the new city
     updateCity(city)
-    .then(data => {
-        console.log(data);
-    }).catch(err => console.log(err));
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
 })
